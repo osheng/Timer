42c9eae12f4f76e2286afe5a11b1b1d2d56d07e9
osheng <osheng@gmail.com>
2021-04-20 16:07:27 -0400

    updated Timer to use clock pulse from Counter
    Things actually work as expected!

--- Timer.v
+++ Timer.v
@@ -3,66 +3,64 @@ This project counts time in decimal and displays it
 on the 7-segment displays.
 It has a pause/continue switch and a reset button.
 
-This project currently works as intended except that
-it seems my 50Mhz clock is roughly only 30Mhz
+Currently the RateDivider module is unnecessary
 
-TODO: Update Counter to output a clock pulse every time it reboots. 
-Then the RateDivider will be unneccessary as a separate module.
 */
+
 module Timer(CLOCK_50,KEY,SW, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 	input [9:0] SW;
 	input [3:0] KEY;
-	input CLOCK_50; 
-	localparam CLOCK_SPEED = 26'd50_000_000;
-	
+	input CLOCK_50;
+
 	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	
-	wire csClock; // Updates every centisecond
-	wire dsClock; // Updates every decisecond
-	wire sClock; //Updates every second 
+	localparam SIZE = 26;
 	
+	wire msClock; // a clock with 1000 cps
+	wire csClock; // a clock with 100 cps
+	wire dsClock; // a clock with 10 cps
+	wire sClock;  // a clock with 1 cps
+	wire s10Clock;// a clock with 1/10 cps
+
+	wire [SIZE - 1:0] garbage;
 	wire [3:0] csCount;
 	wire [3:0] dsCount;
 	wire [3:0] sCount;
+	wire [3:0] s10Count;
 
-	
+
 	wire pauseSwitch;
 	assign pauseSwitch = SW[0];
-	
+
 	wire nResetButton;
 	assign nResetButton = KEY[0];
 	
+	// I would have thought this should be 49_999
+	defparam msCounter.MAX = 26'd499_999;
+	defparam msCounter.SIZE = SIZE;
+	Counter msCounter(.inputClock(CLOCK_50), .pause(pauseSwitch),
+		.nReset(nResetButton), .count(garbage), .outputClock(msClock));
 	
-	
-	RateDivider csRateDivider(.inputClock(CLOCK_50), .pause(pauseSwitch),
-										.nReset(nResetButton), .outputClock(csClock));
-										
-	defparam csRateDivider.MAX_VAL = (CLOCK_SPEED / 26'd100) - 1;
-	
-	Counter csCounter(.clock(csClock), .nReset(nResetButton), .count(csCount));
-							
-	RateDivider dsRateDivider(.inputClock(CLOCK_50), .pause(pauseSwitch),
-										.nReset(nResetButton), .outputClock(dsClock));
-										
-	defparam dsRateDivider.MAX_VAL = (CLOCK_SPEED / 26'd10) - 1;									
-	
-	Counter dsCounter(.clock(dsClock), .nReset(nResetButton), .count(dsCount));
-							
-	RateDivider sRateDivider(.inputClock(CLOCK_50), .pause(pauseSwitch),
-										.nReset(nResetButton), .outputClock(sClock));
-										
-	defparam sRateDivider.MAX_VAL = CLOCK_SPEED - 1;
-	
-	Counter sCounter(.clock(sClock), .nReset(nResetButton),.count(sCount));
-	
+	Counter csCounter(.inputClock(msClock), .pause(pauseSwitch),
+		.nReset(nResetButton), .count(csCount), .outputClock(csClock));
+
+	Counter dsCounter(.inputClock(csClock), .pause(pauseSwitch),
+		.nReset(nResetButton), .count(dsCount), .outputClock(dsClock));
+
+
+	Counter sCounter(.inputClock(dsClock), .pause(pauseSwitch),
+		.nReset(nResetButton), .count(sCount), .outputClock(sClock));
+
+	defparam s10Counter.MAX = 6 - 1;
+	Counter s10Counter(.inputClock(sClock), .pause(pauseSwitch),
+		.nReset(nResetButton), .count(s10Count), .outputClock(s10Clock));
+
 	HexDisplayTranslator csDisplay(.binInput(csCount), .hexDisplayOutput(HEX0));
 	HexDisplayTranslator  dsDisplay(.binInput(dsCount), .hexDisplayOutput(HEX1));
 	HexDisplayTranslator  sDisplay(.binInput(sCount), .hexDisplayOutput(HEX2));
-	
-	
-	assign HEX3 = 7'b111_1111;
+	HexDisplayTranslator  s10Display(.binInput(s10Count), .hexDisplayOutput(HEX3));
 	assign HEX4 = 7'b111_1111;
 	assign HEX5 = 7'b111_1111;
-	
+
 
 endmodule
